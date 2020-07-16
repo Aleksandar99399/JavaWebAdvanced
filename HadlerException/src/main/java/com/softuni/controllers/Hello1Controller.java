a30d9945ec9e9ac6d4d3df284973b1bcba7d8c9f
Aleksandar99399 <aleksandar_unchev@abv.bg>
2020-07-16 12:04:06 +0300

    commit handler

---
+++ HadlerException/src/main/java/com/softuni/controllers/Hello1Controller.java
@@ -0,0 +1,13 @@
+package com.softuni.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Hello1Controller {
+    @GetMapping("/crash1")
+    public ModelAndView crashMe(){
+        throw new HelloException("I crashed");
+    }
+}
