fd0d6b0029461580538a935796113c1bec9445e2
Aleksandar99399 <aleksandar_unchev@abv.bg>
2020-07-16 17:55:20 +0300

    security success

--- workshopTabula/src/main/java/com/tabula/web/AnnouncementController.java
+++ workshopTabula/src/main/java/com/tabula/web/AnnouncementController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @AllArgsConstructor
 @Controller
-@RequestMapping("/announcement")
+@RequestMapping("/announcements")
 public class AnnouncementController {
 
 
